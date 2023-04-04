d6fc74aabc4bd870744c27d36410ab60a94fe569
NwakamaChinedu <nwakama.ec@gmail.com>
2023-04-04 22:00:05 +0100

    set users

--- aboutus.php
+++ aboutus.php
@@ -7,13 +7,12 @@ else{
     readfile('headerLogout.php');
 }
 
-//readfile('headerLogout.php');
-
 ?>
 
 <!DOCTYPE html>
 
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
